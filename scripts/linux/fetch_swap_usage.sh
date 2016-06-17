1d42d040606b6df76c83eeca553e0c98f7c58aeb
liaoaoyang <liaoaoyang@gmail.com>
2016-06-17 15:00:13 +0800

    add script to fetch linux swap usage

---
+++ scripts/linux/fetch_swap_usage.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+pids=`ls /proc/|grep '[0-9]\{1,\}'`
+
+for pid in $pids
+do
+    if [ ! -f "/proc/"$pid"/smaps" ];then
+       	continue
+    fi
+
+    swap_uses=`awk '/^Swap:/ {SWAP+=$2}END{print SWAP" KB"}' "/proc/"$pid"/smaps"`
+    proc=`ps -ef | grep " $pid " | grep -v grep | awk '{for(i=8;i<100;++i){printf "%s ",$i;}printf "\n"}'`
+    echo $pid" "$swap_uses" "$proc
+done
