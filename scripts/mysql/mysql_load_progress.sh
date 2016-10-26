139c6fb1932a2731116327a7e803d8e668516ec8
liaoaoyang <liaoaoyang@gmail.com>
2016-10-27 00:11:23 +0800

    read fsize only one time

--- scripts/mysql/mysql_load_progress.sh
+++ scripts/mysql/mysql_load_progress.sh
@@ -8,11 +8,11 @@ if [ ! -f $LOAD_FILE_NAME ];then
 fi
 
 MYSQL_PIDS=`ps -ef | grep mysql | awk '{print $2}'`
+fsize=`ls -l $LOAD_FILE_NAME | awk '{print $5}'`
 
 for pid in $MYSQL_PIDS
 do
     fd=`lsof -p $pid | grep $LOAD_FILE_NAME | grep -vE "grep|sh -c" | awk '{print $4}' | grep -oP '\d+(?=r)'`
-    fsize=`ls -l $LOAD_FILE_NAME | awk '{print $5}'`
 
     if [ -f  /proc/$pid/fdinfo/$fd ];then
         read_pos=`cat /proc/$pid/fdinfo/$fd  | grep pos | awk "{print \\$2/$fsize*100\"%\"}"`
