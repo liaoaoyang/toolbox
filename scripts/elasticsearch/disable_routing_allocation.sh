414196a9f8fbf8180f0d4888681ad493ae1d8e30
liaoaoyang <liaoaoyang@gmail.com>
2017-01-09 19:00:04 +0800

    reformat code

--- scripts/elasticsearch/disable_routing_allocation.sh
+++ scripts/elasticsearch/disable_routing_allocation.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
+ES_HOST=$1
+
 if [ -z $ES_HOST ]; then
 	echo "Need es host"
 	exit
 fi
 
-curl -s -XPUT "http://$ES_HOST:9200/_cluster/settings" -d'{
+ES_PORT=$2
+
+if [ -z $ES_PORT ]; then
+	echo "Need es port"
+	exit
+fi
+
+curl -s -XPUT "http://$ES_HOST:$ES_PORT/_cluster/settings" -d'{
 	"transient" : {
 		"cluster.routing.allocation.enable" : "none"
-		}
+	}
 }'
