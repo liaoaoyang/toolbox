d54f29e9da469c6dcae9bc1ae8dd8db97988be77
liaoaoyang <liaoaoyang@gmail.com>
2017-07-06 23:56:36 +0800

    added enable_routing_allocation.sh

---
+++ scripts/elasticsearch/enable_routing_allocation.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+ES_HOST=$1
+
+if [ -z $ES_HOST ]; then
+    echo "Need es host"
+    exit
+fi
+
+ES_PORT=$2
+
+if [ -z $ES_PORT ]; then
+    echo "Need es port"
+    exit
+fi
+
+curl -s -XPUT "http://$ES_HOST:$ES_PORT/_cluster/settings" -d'{
+    "transient" : {
+    	"cluster.routing.allocation.enable" : "all"
+    }
+}'
