78a7cc2e9199f5361fbfd225d7388ab3555accf0
liaoaoyang <liaoaoyang@gmail.com>
2017-07-17 15:06:31 +0800

    make output more friendly

--- scripts/linux/fetch_swap_usage.sh
+++ scripts/linux/fetch_swap_usage.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 pids=`ls /proc/|grep '[0-9]\{1,\}'`
+tmp_fn="/tmp/swap_usage_"`date +%s`
 
 for pid in $pids
 do
@@ -8,7 +9,13 @@ do
         continue
     fi
 
-    swap_uses=`awk '/^Swap:/ {SWAP+=$2}END{print SWAP" KB"}' "/proc/"$pid"/smaps"`
+    swap_uses=`awk '/^Swap:/ {SWAP_ORI+=$2}END{SWAP=SWAP_ORI;if(SWAP==""){SWAP_ORI=0;SWAP=0;};if(SWAP>1024*1024){UNIT="GB";SWAP=sprintf("%.2f",SWAP/1024.0/1024);}else if (SWAP>1024){UNIT="MB";SWAP=sprintf("%.2f",SWAP/1024.0);}else{UNIT="KB"};print "TAG "SWAP_ORI" GAT "SWA
+P""UNIT}' "/proc/"$pid"/smaps"`
     proc=`ps -ef | grep " $pid " | grep -v grep | awk '{for(i=8;i<100;++i){printf "%s ",$i;}printf "\n"}'`
-    echo $pid" "$swap_uses" "$proc
+    echo $pid" "$swap_uses" "$proc >> $tmp_fn
 done
+
+if [ -f $tmp_fn ]; then
+    cat $tmp_fn | sort -nrk3 | sed -r 's/TAG\s[0-9]+\sGAT\s//'
+    rm -f $tmp_fn
+fi
