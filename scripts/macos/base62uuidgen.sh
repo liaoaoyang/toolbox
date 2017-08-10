41eb784d45ceb8fc7f460e66c94622bac4e8ee9d
liaoaoyang <liaoaoyang@gmail.com>
2017-08-10 10:23:37 +0800

    format code style

--- scripts/macos/base62uuidgen.sh
+++ scripts/macos/base62uuidgen.sh
@@ -5,11 +5,11 @@
 uuidgen | tr "a-f" "A-F" | tr -d "-" | xargs -I "{}" echo "obase=62;ibase=16;{}" | bc | sed 's/0\([0-9]\)/\1/g' | awk 'BEGIN{r=""}{
 	for(i = 1; i <= NF; ++i) {
 		if ($i < 10) {
-			r=r""$i;
+			r = r""$i;
 		} else if ($i < 36) {
-			r=sprintf("%s%c", r, ($i - 10 + 97));
+			r = sprintf("%s%c", r, ($i - 10 + 97));
 		} else {
-			r=sprintf("%s%c", r, ($i - 36 + 65));
+			r = sprintf("%s%c", r, ($i - 36 + 65));
 		}
 	}
 }
