fcf697033788df06d886160d84f65a0a61cb3999
liaoaoyang <liaoaoyang@gmail.com>
2019-09-24 22:58:42 +0800

    make params configuratable

--- scripts/linux/pre_pub_jpg.sh
+++ scripts/linux/pre_pub_jpg.sh
@@ -1,13 +1,37 @@
 #!/bin/sh
 
-wm_per=0.12
-wm_fn=~/Tools/watermask.png
-w_delta=20 
-h_delta=20 
-out_suffix="-out"
-out_width=1
-out_quality=95
-ext_opts="-strip"
+if [ -z $wm_per ]; then
+	wm_per=0.12
+fi
+
+if [ -z $wm_fn ]; then
+	wm_fn=~/Tools/watermask.png
+fi
+
+if [ -z $w_delta ]; then
+	w_delta=20 
+fi
+
+if [ -z $h_delta ]; then
+	h_delta=20 
+fi
+
+if [ -z $out_suffix ]; then
+	out_suffix="-out"
+fi
+
+if [ -z $out_width ]; then
+	out_width=1
+fi
+
+if [ -z $out_quality ]; then
+	out_quality=95
+fi
+
+if [ -z $ext_opts ]; then
+	ext_opts="-strip"
+fi
+
 for src_fn in `ls | awk '/(jpg|JPG)$/ {print $1}' | grep -vE '_ppj_tmp|out|watermask'` 
 do 
 	src_fnn=`echo $src_fn | sed 's/.jpg//' | sed 's/.JPG//'`
